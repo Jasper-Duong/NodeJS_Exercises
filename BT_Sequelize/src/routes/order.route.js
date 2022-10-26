1d4a5db7092e8de0dd853baafd8d5e4434b2816c
Jasper-Duong <duongngochung13@gmail.com>
2022-10-26 17:09:47 +0700

    Finished Sequlize exercise

--- BT_Sequelize/src/routes/order.route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { addOrder } = require('../controllers/order.control');
-const orderRoute = express.Router();
-
-orderRoute.post('/addOrder', addOrder);
-
-module.exports = orderRoute
